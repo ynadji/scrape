4af7de93fb370a02fdae4870bc5df692da77295e
Yacin Nadji <yacin@gatech.edu>
2011-02-11 09:43:59 -0500

    Parse thumbnails properly

--- slib.py
+++ slib.py
@@ -1,9 +1,21 @@
 from scrape import *
 import sys,os
 
-def basic(region, tag, attr, content=None, usealltags=False):
+def basic(region, tag, attr, content=None, usealltags=False, parsethumb=False):
     if usealltags:
         alltags = region.alltags(tag)
+        # If we are parsing img with usealltags, we may want to grab the linked
+        # image if its a thumbnail.
+        if parsethumb:
+            fullsize = []
+            for t in alltags:
+                try:
+                    fullsize.append(t.enclosing('a'))
+                except ScrapeError:
+                    pass
+            alltags = fullsize
+            # Now we want the enclosing anchor tag's href (link to fullsize).
+            attr = 'href'
     else:
         alltags = region.all(tag, content)
     for r in alltags:
